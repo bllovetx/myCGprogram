bbd80edad0021d1e8ab548ca13fa5912746922a7
曾许曌秋 <xzqzeng@gmail.com>
2021-05-29 21:33:10 +0800

    modify test

--- source/testing/testqt5.py
+++
@@ -1,9 +0,0 @@
-def mix_point(pA:list, pB:list, t:float):
-    assert(len(pA) == len(pB))
-    mixed = []
-    for i in range(len(pA)):
-        mixed.append(t*pA[i] + (1-t)*pB[i])
-    return mixed
-
-
-print(mix_point([1, 3], [2, 6], 0.2))
